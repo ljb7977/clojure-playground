8ddcbdc99ef46e29cc28f1329998df2edc95e4f4
Jubeen Lee <ljb7977@gmail.com>
2021-12-07 19:01:02 +0900

    2-2: Use destruction instead of let binding

--- src/aoc2.clj
+++ src/aoc2.clj
@@ -30,16 +30,16 @@
      (apply map +)
      multiply)
 ; ------- Part 2 -------
-(defn get-permutation [input]
-  (for [a input, b input] [a b]))
+(defn get-permutation [xs]
+  (for [a xs, b xs] [a b]))
 
 (defn get-hamming-distance [a b]
-  (loop [distance 0, a a, b b]
-    (let [_a (first a), _b (first b)]
+  "Example: asdf, asff -> 1"
+  (loop [distance 0, [first-a & rest-a] a, [first-b & rest-b] b]
       (cond
-        (or (nil? _a) (nil? _b)) distance
-        (= _a _b) (recur distance (rest a) (rest b))
-        :else (recur (inc distance) (rest a) (rest b))))))
+        (or (nil? first-a) (nil? first-b)) distance
+        (= first-a first-b) (recur distance rest-a rest-b)
+        :else (recur (inc distance) rest-a rest-b))))
 
 (defn find-correct-boxes [boxes]
   (let [[a b] (first boxes)]
@@ -48,14 +48,14 @@
       (recur (rest boxes)))))
 
 (defn get-common-letters [[a b]]
-  (loop [commons "", a a, b b]
-    (let [_a (first a), _b (first b)]
+  (loop [commons "", [first-a & rest-a] a, [first-b & rest-b] b]
       (cond
-        (or (nil? _a) (nil? _b)) commons
-        (= _a _b) (recur (str commons _a) (rest a) (rest b))
-        :else (recur commons (rest a) (rest b))))))
+        (or (nil? first-a) (nil? first-b)) commons
+        (= first-a first-b) (recur (str commons first-a) rest-a rest-b)
+        :else (recur commons rest-a rest-b))))
 
-(->> input-val
-     get-permutation
-     find-correct-boxes
-     get-common-letters)
+(comment
+  (->> input-val
+       get-permutation
+       find-correct-boxes
+       get-common-letters))
