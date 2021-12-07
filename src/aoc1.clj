8ceb24c33eb9dfeeea8dd928b80ff74dc5b74eb9
Jubeen Lee <ljb7977@gmail.com>
2021-12-07 16:42:34 +0900

    Use loop in 1-2

--- src/aoc1.clj
+++ src/aoc1.clj
@@ -12,13 +12,14 @@
 (reduce + input-seq)
 
 ; -------- Part 2 ----------
-(defn iter [history sum seq]
-  (let [addend (first seq)
-        sum (+ sum addend)]
-    (if (contains? history sum)
-      sum
-      (recur (conj history sum) sum (rest seq)))))
+(defn iter [seq]
+  (loop [history #{0}, sum 0, seq seq]
+    (let [addend (first seq)
+          sum (+ sum addend)]
+      (if (contains? history sum)
+        sum
+        (recur (conj history sum) sum (rest seq))))))
 
 (->> input-seq
      cycle
-     (iter #{0} 0))
+     iter)
