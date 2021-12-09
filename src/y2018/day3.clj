3650cd4387d4f7711138ff5700a8feea52bb5fb2
Jubeen Lee <ljb7977@gmail.com>
2021-12-09 15:19:24 +0900

    Remove unused function in day3

--- src/y2018/day3.clj
+++ src/y2018/day3.clj
@@ -14,23 +14,8 @@
    :x-size x-size
    :y-size y-size})
 
-(defn parse-one-line
-  "입력의 한 줄을 {:idx :x :y :x-size :y-size}의 키를 가지는 맵으로 파싱합니다.
-  Example)
-  Input: #1 @ 912,277: 27x20
-  Output: {:idx 1, :x 912, :y 277, :dx 27, :dy 20}
-  "
-  [line]
-  (->> line
-       (re-seq pattern)
-       flatten
-       rest
-       (map util/parse-int)
-       input-list->map))
-
 (defn get-covered-points
   "x, y, x-size, y-size에 기반하여 해당 fabric이 덮는 점들의 좌표 리스트를 구합니다.
-  Example)
   Input: {:x 1 :y 3 :x-size 3 :y-size 2}
   Output: ([1 3] [1 4] [2 3] [2 4] [3 3] [3 4])
   "
@@ -48,11 +33,16 @@
 (defn process-one-step [f]
   (->> f
        (re-seq pattern)
-       flatten
+       first
        rest
        (map util/parse-int)
        input-list->map
        draw))
+
+; Test process-one-step
+(comment
+  (->> "#1 @ 1,2: 3x2"
+       process-one-step))
 
 ;------- Part 1 -------
 (comment
