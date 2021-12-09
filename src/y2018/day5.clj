7bae00e7b886db7b76c57661749d40af4d01ace8
Jubeen Lee <ljb7977@gmail.com>
2021-12-09 20:47:28 +0900

    Save

--- src/y2018/day5.clj
+++ src/y2018/day5.clj
@@ -3,3 +3,28 @@
 (def input-val
   (slurp "resources/y2018/day5.txt"))
 
+(defn abs [n]
+  (max n (- n)))
+
+(defn character-matched? [f g]
+  (= (abs (- (int f) (int g))) 32))
+
+(defn remove-matching-pairs [stream]
+  (reduce (fn [acc val]
+            (let [last-char (peek acc)]
+              (cond
+                (nil? last-char) [val]
+                (character-matched? last-char val) (pop acc)
+                :else (conj acc val))))
+          []
+          stream))
+
+(defn loop-removing [s]
+  (loop [prev s]
+    (let [removed (remove-matching-pairs prev)]
+      (if (= removed prev) removed
+                           (recur removed)))))
+
+(->> input-val
+     loop-removing
+     (apply str))
