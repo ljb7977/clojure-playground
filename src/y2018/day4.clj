4a1f0ca1fa4496341799a6f16fd04fcbfd734055
Jubeen Lee <ljb7977@gmail.com>
2021-12-10 16:46:38 +0900

    Add docstrings

--- src/y2018/day4.clj
+++ src/y2018/day4.clj
@@ -3,6 +3,8 @@
 
 (def input-val
   (util/read-lines "y2018/day4.txt"))
+;(def input-val
+;  (util/read-lines "y2018/day4-example.txt"))
 (def pattern #"\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] (wakes up|falls asleep|Guard #(\d+) begins shift)")
 
 ; Integer/parseInt 한번에 할 수 있는 방법이 없을까?
@@ -15,16 +17,38 @@
    :action   action
    :guard-id (if-let [guard-id guard-id] (Integer/parseInt guard-id) nil)})
 
-(defn parse [f]
+(defn parse
+  "문제의 입력을 파싱하여 action 하나의 정보를 담는 map으로 만듭니다.
+  Input: [1518-02-06 23:51] Guard #2081 begins shift
+  Output: {:year 1518 :month 2 :day 6 :hour 23 :minute 51 :action \"Guard #2081 begins shift\" :guard-id 2081}
+
+  Input: [1518-05-12 00:39] wakes up
+  Output: {:year 1518 :month 5 :day 12 :hour 0 :minute 39 :action \"wakes up\" :guard-id nil}
+  "
+  [f]
   (->> f
        (re-seq pattern)
-       flatten
+       first
        rest
        ->action-record-map))
 
 ; 자료구조가 불안불안하다...
-(defn records->sleeping-ranges [{:keys [result curr]}
-                                {:keys [action month day minute guard-id]}]
+(defn records->sleeping-ranges
+  "위에서 parsing한 map 리스트를 가지고, 잠에 든 시간부터 일어나는 시간까지를 한 묶음으로 묶어 줍니다.
+  Input: {:result [] :curr {}}, {:action \"Guard #2081 begins shift\" :month 2 :day 6 :hour 23 :minute 51 :guard-id 2081}
+  Output: {:result [] :curr {:guard-id 2081}}
+
+  Input: {:result [] :curr {:guard-id 2081}}, {:action \"falls asleep\" :month 2 :day 7 :hour 0 :minute 1 :guard-id nil}
+  Output: {:result [] :curr {:guard-id 2081 :month 2 :day 7 :start-minute 1}}
+
+
+  Input: {:result [] :curr {:guard-id 2081 :month 2 :day 7 :start-minute 1}},
+         {:action \"wakes up\" :month 2 :day 7 :hour 0 :minute 10 :guard-id nil}
+  Output: {:result [{:guard-id 2081 :month 2 :day 7 :start-minute 1 :end-minute 10}]
+           :curr {:guard-id 2081 :month 2 :day 7 :start-minute 1 :end-minute 10}}
+  "
+  [{:keys [result curr]}
+   {:keys [action month day minute guard-id]}]
   (let [updated-curr (cond
                        (not (nil? guard-id)) {:guard-id guard-id}
                        (= action "falls asleep") {:guard-id     (curr :guard-id)
@@ -41,16 +65,29 @@
       {:result result :curr updated-curr})))
 
 (defn collect-minutes
-  "Input: {1 '(1 2 3), 2 '(10 11 12)}, {:guard-id 1 :start-minute 6 :end-minute 10}
-  Output: {1 (1 2 3 6 7 8 9), 2 (10 11 12)}
+  "
+  가드의 id -> 그 가드가 잠들었던 분(minute)들의 리스트 맵을 업데이트합니다.
+  Input: {1 '(1 2 3), 2 '(10 11 12)}, {:guard-id 1 :start-minute 6 :end-minute 10}
+  Output: {1 '(1 2 3 6 7 8 9), 2 '(10 11 12)}
   "
   [minutes {:keys [guard-id start-minute end-minute]}]
   (update minutes guard-id concat (range start-minute end-minute)))
 
-(defn get-frequencies [[id xs]]
+(defn get-frequencies
+  "
+  Input: [1 '(1 1 2 3)]
+  Output: [1 {1 2, 2 1, 3 1}]
+  "
+  [[id xs]]
   [id (frequencies xs)])
 
-(defn max-val-of-map [m]
+(defn max-val-of-map
+  "
+  주어진 map의 value 중에서 가장 큰 값을 반환합니다.
+  Input: {1 2 2 3 3 4}
+  Output: 4
+  "
+  [m]
   (apply max (vals m)))
 
 (comment
