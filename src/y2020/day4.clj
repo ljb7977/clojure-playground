3d7bb475debbc9fdcc1ff8afd7147668f9bc1bf7
Jubeen Lee <ljb7977@gmail.com>
2021-12-13 15:49:04 +0900

    2020 day4-1 완성

--- src/y2020/day4.clj
+++ src/y2020/day4.clj
@@ -2,19 +2,20 @@
   (:require util))
 
 (def input-val
-  (util/read-lines "y2020/day4-example.txt"))
-  ;(util/read-lines "y2020/day4.txt"))
+  ;(util/read-lines "y2020/day4-example.txt")
+  (util/read-lines "y2020/day4.txt"))
 
 (def valid-passport-keys
   #{:byr :iyr :eyr :hgt :hcl :ecl :pid})
 
-(defn group-strings [xs]
+(defn split-strings-by-empty-one [xs]
   (->> xs
        (reduce (fn [{:keys [result prev]} curr]
                  (if (= "" curr) {:result (conj result prev) :prev ""}
                                  {:result result :prev (str prev curr " ")}))
                {:result [] :prev ""})
        :result))
+; split-with을 써서 할 수 있는 방법이 어떻게 될까?
 
 (defn ->passport-map [xs]
   (->> xs
@@ -23,12 +24,20 @@
        (into (sorted-map))))
 (->passport-map ["hcl:#bc352c" "pid:321838059" "byr:1930" "hgt:178cm" "cid:213" "eyr:2023" "ecl:amb" "iyr:2017"])
 
+(defn parse [x]
+  (->> x
+       vec
+       (#(conj % ""))
+       split-strings-by-empty-one
+       (map #(clojure.string/split % #" "))
+       (map ->passport-map)))
+
 (defn validate-passport [m]
   (every? (set (keys m)) valid-passport-keys))
 
-
 (comment
   (->> input-val
-       group-strings
-       (map #(clojure.string/split % #" "))
-       (map ->passport-map)))
+       parse
+       (map validate-passport)
+       (filter true?)
+       count))
