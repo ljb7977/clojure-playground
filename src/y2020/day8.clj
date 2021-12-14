3096bc247228a9da4568e13b4f9e612495031687
Jubeen Lee <ljb7977@gmail.com>
2021-12-15 06:20:40 +0900

    Renaming

--- src/y2020/day8.clj
+++ src/y2020/day8.clj
@@ -8,20 +8,22 @@
   (let [[op num] (clojure.string/split x #" ")]
     [(keyword op) (Integer/parseInt num)]))
 
-(defn state-machine-one-step [{:keys [acc op-id ops]}]
+(defn next-step [{:keys [acc op-id ops]}]
   (let [[op arg] (nth ops op-id)]
     (case op
-      :acc {:acc (+ acc arg) :next-op-id (inc op-id)}
-      :nop {:acc acc :next-op-id (inc op-id)}
-      :jmp {:acc acc :next-op-id (+ op-id arg)})))
+      :acc {:acc (+ acc arg) :op-id (inc op-id)}
+      :nop {:acc acc :op-id (inc op-id)}
+      :jmp {:acc acc :op-id (+ op-id arg)})))
 
 (defn do-until-loop-detected [ops]
   (loop [acc 0 op-id 0 op-visited? #{}]
-    (let [{new-acc :acc next-op-id :next-op-id} (state-machine-one-step {:acc acc :op-id op-id :ops ops})]
-      (if (op-visited? next-op-id)
+    (let [{new-acc :acc new-op-id :op-id} (next-step {:acc acc :op-id op-id :ops ops})]
+      (if (op-visited? new-op-id)
         acc
-        (recur new-acc next-op-id (conj op-visited? op-id))))))
+        (recur new-acc new-op-id (conj op-visited? op-id))))))
 
-(comment (let [ops (->> input-val
-                        (map parse))]
-           (do-until-loop-detected ops)))
+(comment
+  ; Part 1
+  (let [ops (->> input-val
+                 (map parse))]
+    (do-until-loop-detected ops)))
