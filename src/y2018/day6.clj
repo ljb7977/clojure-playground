03eccbbb266b1e739a380d4a1b666a801e55670e
Jubeen Lee <ljb7977@gmail.com>
2021-12-14 14:52:56 +0900

    Rename key

--- src/y2018/day6.clj
+++ src/y2018/day6.clj
@@ -45,17 +45,17 @@
     {:location [3 2], :coord-id 3, :distance 2}
     {:location [3 2], :coord-id 4, :distance 5}
     {:location [3 2], :coord-id 5, :distance 12}]]
-  Output: {:location [3 2] :closest-coord-ids (2 3)}
+  Output: {:location [3 2] :closest-coord-id (2 3)}
   "
   [[location ms]]
   (let [shortest-distance (->> ms
                                (map :distance)
                                (apply min))
-        closest-coord-ids (->> ms
-                               (filter #(= (:distance %) shortest-distance))
-                               (map :coord-id))]
-    {:location          location
-     :closest-coord-ids closest-coord-ids}))
+        closest-coord-id (->> ms
+                              (filter #(= (:distance %) shortest-distance))
+                              (map :coord-id))]
+    {:location         location
+     :closest-coord-id closest-coord-id}))
 
 (defn point-at-border? [[x y] {:keys [xmin ymin xmax ymax]}]
   (or (= x xmin) (= y ymin) (= x xmax) (= y ymax)))
@@ -68,19 +68,20 @@
         location-and-its-closest-coord (->> locations-to-coords
                                             (group-by :location)
                                             (map keep-closest-coords)
-                                            (filter (fn [{:keys [location closest-coord-ids]}]
-                                                      (= 1 (count closest-coord-ids))))
-                                            (map #(update % :closest-coord-ids first)))
+                                            (filter (fn [{:keys [location closest-coord-id]}]
+                                                      (= 1 (count closest-coord-id))))
+                                            (map #(update % :closest-coord-id first)))
         locations-at-border (->> location-and-its-closest-coord
                                  (filter (fn [{:keys [location]}]
                                            (point-at-border? location border))))
         coord-ids-that-reach-border (->> locations-at-border
-                                         (map :closest-coord-ids)
+                                         (map :closest-coord-id)
                                          set)]
 
+    ; Part 1
     (->> location-and-its-closest-coord
-         (filter (fn [{:keys [closest-coord-ids]}] (not (coord-ids-that-reach-border closest-coord-ids))))
-         (map :closest-coord-ids)
+         (filter (fn [{:keys [closest-coord-id]}] (not (coord-ids-that-reach-border closest-coord-id))))
+         (map :closest-coord-id)
          frequencies
          (map second)
          (apply max))))
