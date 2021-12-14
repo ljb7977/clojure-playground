ed11ff1a9fc84951fb0a2bf11ac37ee2bb4296d6
Jubeen Lee <ljb7977@gmail.com>
2021-12-14 20:29:45 +0900

    Spec 사용하여 part 2 해결

--- src/y2020/day4.clj
+++ src/y2020/day4.clj
@@ -5,20 +5,37 @@
 (def input-val
   (-> (slurp "resources/y2020/day4.txt")
       (clojure.string/split #"\n\n")))
-;SPEC!!!!
+
+(defn parse-int-if-available [x]
+  (try
+     (Integer/parseInt x)
+     (catch Exception e x)))
+
 
 (def required-keys #{:byr :iyr :eyr :hgt :hcl :ecl :pid})
+
+(defn parse-values [{:keys [passport/byr passport/cid passport/ecl passport/eyr
+                            passport/hcl passport/hgt passport/iyr passport/pid]}]
+  #:passport{:byr (parse-int-if-available byr)
+             :iyr (parse-int-if-available iyr)
+             :eyr (parse-int-if-available eyr)
+             :pid pid
+             :hcl hcl
+             :hgt hgt
+             :ecl (keyword ecl)
+             :cid cid})
 
 (defn ->passport-map [xs]
   "
   key:value 형태의 string의 sequence로 들어오는 입력값을 map 형태로 변환합니다.
   Input: [\"hcl:#bc352c\" \"pid:321838059\" \"byr:1930\" \"hgt:178cm\" \"cid:213\" \"eyr:2023\" \"ecl:amb\" \"iyr:2017\"]
-  Output: {:byr \"1930\", :cid \"213\", :ecl \"amb\", :eyr \"2023\", :hcl \"#bc352c\", :hgt \"178cm\", :iyr \"2017\", :pid \"321838059\"}
+  Output: #:passport{:byr \"1930\", :cid \"213\", :ecl \"amb\", :eyr \"2023\", :hcl \"#bc352c\", :hgt \"178cm\", :iyr \"2017\", :pid \"321838059\"}
   "
   (->> xs
        (map #(clojure.string/split % #":"))
-       (map (fn [[k v]] [(keyword k) v]))
-       (into (sorted-map))))
+       (map (fn [[k v]] [(keyword "passport" k) v]))
+       (into (sorted-map))
+       parse-values))
 ;(->passport-map ["hcl:#bc352c" "pid:321838059" "byr:1930" "hgt:178cm" "cid:213" "eyr:2023" "ecl:amb" "iyr:2017"])
 
 (defn parse [x]
@@ -29,36 +46,29 @@
 (defn has-required-keys? [m]
   (every? (set (keys m)) required-keys))
 
-(def valid-eye-colors
-  #{:amb :blu :brn :gry :grn :hzl :oth})
+(s/def :passport/eye-colors #{:amb :blu :brn :gry :grn :hzl :oth})
 
-(defn integer-in-range? [s start end]
-  (if-let [num (Integer/parseInt s)]
-    (<= start num end)
+(defn num-in-range? [num start end]
+  (<= start num end))
+
+(defn valid-height? [s]
+  (if-let [[[_, num, unit]] (re-seq #"^(\d+)(cm|in)$" s)]
+    (let [num (Integer/parseInt num)]
+      (cond
+        (= unit "cm") (<= 150 num 193)
+        (= unit "in") (<= 59 num 76)))
     false))
 
-(def validators
-  {:byr #(integer-in-range? % 1920 2002)
-   :iyr #(integer-in-range? % 2010 2020)
-   :eyr #(integer-in-range? % 2020 2030)
-   :hgt #(if-let [[[_, num, unit]] (re-seq #"^(\d+)(cm|in)$" %)]
-           (let [num (Integer/parseInt num)]
-             (cond
-               (= unit "cm") (<= 150 num 193)
-               (= unit "in") (<= 59 num 76)))
-           false)
-   :hcl #(re-matches #"^#[a-f|0-9]{6}$" %)
-   :ecl #(valid-eye-colors (keyword %))
-   :pid #(re-matches #"^\d{9}$" %)
-   :cid any?})
-
-(defn get-validator-and-validate [[k v]]
-  (if-let [validator (k validators)]
-    (validator v)))
-
-(defn valid-passport? [m]
-  (and (has-required-keys? m)
-       (every? get-validator-and-validate m)))
+(s/def :passport/byr (s/and int? #(num-in-range? % 1920 2002)))
+(s/def :passport/iyr (s/and int? #(num-in-range? % 2010 2020)))
+(s/def :passport/eyr (s/and int? #(num-in-range? % 2020 2030)))
+(s/def :passport/hgt (s/and string? valid-height?))
+(s/def :passport/hcl (s/and string? #(re-matches #"^#[a-f|0-9]{6}$" %)))
+(s/def :passport/ecl (s/and keyword? #(s/valid? :passport/eye-colors %)))
+(s/def :passport/pid (s/and string? #(re-matches #"^\d{9}$" %)))
+(s/def :passport/passport (s/keys :req [:passport/byr :passport/iyr :passport/eyr :passport/hgt
+                                        :passport/hcl :passport/ecl :passport/pid]
+                                  :opt [:passport/cid]))
 
 (comment
   ; Part 1
@@ -70,5 +80,5 @@
   ; Part 2
   (->> input-val
        parse
-       (filter valid-passport?)
+       (filter #(s/valid? :passport/passport %))
        count))
