79774121cb036526e964793c2f5ccaf71f71e6ed
Jubeen Lee <ljb7977@gmail.com>
2021-12-16 20:25:53 +0900

    7-2 완료

--- src/y2018/day7.clj
+++ src/y2018/day7.clj
@@ -2,7 +2,7 @@
   (:require util))
 
 (def input-val
-  (util/read-lines "y2018/day7-example.txt"))
+  (util/read-lines "y2018/day7.txt"))
 
 (defn parse-one-line
   "Input: Step C must be finished before step A can begin.
@@ -14,19 +14,19 @@
      :removed-keys (first result)}))
 
 (defn ->graph
-  "Input: [{:precond \\C, :result \\A}
-           {:precond \\C, :result \\F}
-           {:precond \\A, :result \\B}
-           {:precond \\A, :result \\D}
-           {:precond \\B, :result \\E}
-           {:precond \\D, :result \\E}
-           {:precond \\F, :result \\E}]
-  Output: {\\A #{\\C}, \\B #{\\A}, \\C #{}, \\D #{\\A}, \\E #{\\B \\D \\F}, \\F #{\\C}}"
+  "Input: [{:precond C, :result A}
+           {:precond C, :result F}
+           {:precond A, :result B}
+           {:precond A, :result D}
+           {:precond B, :result E}
+           {:precond D, :result E}
+           {:precond F, :result E}]
+  Output: {A #{C}, B #{A}, C #{}, D #{A}, E #{B D F}, F #{C}}"
   [xs]
   (let [chars (->> xs
-                  (map vals)
-                  flatten
-                  set)]
+                   (map vals)
+                   flatten
+                   set)]
     (reduce (fn [acc {:keys [precond removed-keys]}]
               (update acc removed-keys #(conj % precond)))
             (zipmap chars (repeat #{}))
@@ -46,15 +46,19 @@
        (filter #(empty? (val %)))
        (map key)))
 
-(defn remove-value
-  "맵의 value인 set들에서 특정 value를 모두 제거합니다.
+(defn remove-values
+  "맵의 value인 set들에서 특정 value들을 모두 제거합니다.
   Input: {A #{C}, B #{C}, C #{}}, C
   Output: {A #{}, B #{}, C #{}}
   "
-  [m value]
+  [m values]
   (->> m
-       (map (fn [[k v]] [k (disj v value)]))
+       (map (fn [[k v]] [k (apply disj v values)]))
        (into {})))
+
+(defn remove-value
+  [m value]
+  (remove-values m [value]))
 
 (defn process-one-step
   "Input: {:result [] :graph {A #{C}, B #{A}, C #{}, D #{A}, E #{B D F}, F #{C}}}
@@ -74,41 +78,91 @@
 
 ;(process-one-step {:result [], :graph {\A #{\C}, \B #{\A}, \C #{}, \D #{\A}, \E #{\B \D \F}, \F #{\C}}})
 
-(defn find-idle-worker [workers]
-  (->> workers
-       (keep-indexed #(if (nil? %2) %1))
-       first))
-;(find-idle-worker [1 nil 2 3 nil])
-
-;worker: {:job \A :remaining 5}
-(defn process-workers [workers]
-  (mapv (fn [{:keys [job remaining]}]
-          (if (= 1 remaining)
-            nil
-            {:job job :remaining (dec remaining)})) workers))
-(process-workers [{:job \A :remaining 2} {:job \B :remaining 1}])
-
-; 미완성.
 ; 로직:
 ; 1. 모든 워커들에 대해 1초식 진행시켜준다.
-; 2. 끝난 워커가 있으면, 그 워커를 회수한다. (nil로 변경)
+;worker: {:job \A :remaining 5}
+(defn process-workers
+  "Input: [{:job A :remaining 5} {:job B :remaining 1}]
+  Output: [{:job A :remaining 4} {:job B :remaining 0}]"
+  [workers]
+  (mapv (fn [{:keys [job remaining]}]
+            {:job       job
+             :remaining (dec remaining)}) workers))
+; ----
+(defn get-finished-jobs
+  "Input: [{:job A :remaining 0} {:job B :remaining 1}]
+  Output: (A)"
+  [workers]
+  (->> workers
+       (filter #(= 0 (:remaining %)))
+       (map :job)))
+
+(defn empty-finished-workers
+  "Input: [{:job A :remaining 0} {:job B :remaining 1}]
+  Output: [{:job B :remaining 1}]"
+  [workers]
+  (remove (fn [{remaining :remaining}] (= remaining 0)) workers))
+
+
+; 2. 끝난 워커가 있으면, 그 job을 회수한다. (nil로 변경)
+(defn reap-finished-job
+  "Input: [{:job A :remaining 0} {:job B :remaining 1}]
+  Output: {:workers [{:job B :remaining 1}]
+           :finished-jobs [A]}"
+  [workers]
+  {:workers       (empty-finished-workers workers)
+   :finished-jobs (get-finished-jobs workers)})
+
+(defn get-duration-of-job [job]
+  (+ 61 (- (int job) (int \A))))
+
+(defn add-job-to-workers [workers job]
+  (conj workers {:job job :remaining (get-duration-of-job job)}))
+
+(defn job-already-working? [workers job]
+  (->> workers
+       (map :job)
+       set
+       (#(contains? % job))))
+
+(defn assign-jobs-to-workers [workers graph]
+  (let [assignable-jobs (->> graph
+                             get-keys-where-value-is-empty
+                             (sort compare))]
+    (reduce (fn [acc val]
+              (cond
+                (= (count acc) 5) (reduced acc)
+                (job-already-working? acc val) acc
+                :else (add-job-to-workers acc val)))
+            workers
+            assignable-jobs)))
+
 ; 3. 처리 완료된 알파벳들을 done 리스트에 추가한다.
 ; 4. 그래프에서 그 알파벳들을 제거한다.
 ; 5. 넣을 수 있는 만큼 새 알파벳을 워커에 넣는다.
-; 이하 반복
-;(defn process-one-step-with-workers [{:keys [result graph workers] :as all}]
-;  (if
-;    (and (every? nil? workers) (empty? graph)) all
-;    (let [keys-with-empty-list (get-keys-where-value-is-empty-list graph)
-;          min-key-with-empty-list (apply min-key int keys-with-empty-list)
-;          workers (process-workers workers)
-;          idle-worker-id (find-idle-worker workers)]
-;      (if (not (nil? idle-worker-id)))
-;      (-> graph
-;          (dissoc min-key-with-empty-list)
-;          (remove-value-from-all-entry min-key-with-empty-list)
-;          ((fn [g]
-;             {:result (conj result min-key-with-empty-list) :graph g}))))))
+(defn process-step-with-workers [{:keys [graph workers result]}]
+  (let [{finished-jobs :finished-jobs
+         workers-after-reaping :workers} (->> workers
+                                              process-workers
+                                              reap-finished-job)
+        new-graph (as-> graph v
+                      (apply dissoc v finished-jobs)
+                      (remove-values v finished-jobs))
+        new-workers (assign-jobs-to-workers workers-after-reaping new-graph)]
+    {:graph new-graph
+     :workers new-workers
+     :result (concat result finished-jobs)}))
+
+(process-step-with-workers {:graph {\A #{\C}, \B #{\A}, \C #{}, \D #{\A}, \E #{\B \D \F}, \F #{\C}}
+                            :workers []
+                            :result []})
+(def graph {\A #{\C}, \B #{\A}, \C #{}, \D #{\A}, \E #{\B \D \F}, \F #{\C}})
+(->> []
+     process-workers
+     reap-finished-job)
+(as-> graph v
+    (apply dissoc v [])
+    (remove-values v []))
 
 (defn get-result-as-string [{result :removed-keys}]
   (apply str result))
@@ -120,4 +174,11 @@
        (#(iterate process-one-step {:removed-keys [] :graph %}))
        (drop-while #(not (empty? (:graph %))))
        first
-       get-result-as-string))
+       get-result-as-string)
+
+  (->> input-val
+       parse
+       (#(iterate process-step-with-workers {:graph % :workers [] :result []}))
+       (drop 1)
+       (take-while #(or (seq (:workers %)) (seq (:graph %))))
+       count))
