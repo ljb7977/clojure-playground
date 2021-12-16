10b4eb55ee323410e4576545a827dbeb2659e86b
Jubeen Lee <ljb7977@gmail.com>
2021-12-16 14:02:52 +0900

    iterate를 써서 리팩토링

--- src/y2020/day8.clj
+++ src/y2020/day8.clj
@@ -6,60 +6,29 @@
 
 (defn parse [x]
   (let [[op num] (clojure.string/split x #" ")]
-    [(keyword op) (Integer/parseInt num)]))
+    {:op (keyword op)
+     :arg (Integer/parseInt num)}))
 
 (defn next-step
   "State machine을 한 step 계산한 결과를 돌려줍니다.
-  Input: 0 0 [[:acc 10] [:jmp -1]]
-  Output: {:acc 10 :op-id 1}
+  Input: {:acc 0 :op-id 0 :ops [{:op :acc :arg 10} {:op :jmp :arg -1}] :history #{}}
+  Output: {:acc 10 :op-id 1 :ops [{:op :acc :arg 10} {:op :jmp :arg -1}] :history #{0}}
   "
-  [{:keys [acc op-id ops]}]
-  (let [[op arg] (nth ops op-id)]
+  [{:keys [acc op-id ops history]}]
+  (let [{:keys [op arg]} (nth ops op-id)]
     (case op
-      :acc {:acc (+ acc arg) :op-id (inc op-id)}
-      :nop {:acc acc :op-id (inc op-id)}
-      :jmp {:acc acc :op-id (+ op-id arg)})))
-
-(defn do-until-loop-detected
-  "loop이 발견될 때까지 state machine을 실행하고, 루프에 들어갔을 때의 acc 값을 반환합니다.
-  Input: [[:acc 10] [:jmp -1]]
-  Output: 10
-  "
-  [ops]
-  (loop [acc 0 op-id 0 op-visited? #{}]
-    (let [{new-acc :acc new-op-id :op-id} (next-step {:acc acc :op-id op-id :ops ops})]
-      (if (op-visited? new-op-id)
-        acc
-        (recur new-acc new-op-id (conj op-visited? op-id))))))
-
-;reduce 버전
-(defn do-until-loop-detected
-  [ops]
-  (reduce (fn [{:keys [acc op-id op-visited?]} _]
-            (let [{new-acc :acc new-op-id :op-id} (next-step {:acc acc :op-id op-id :ops ops})]
-              (if (op-visited? new-op-id)
-                (reduced acc)
-                {:acc new-acc :op-id new-op-id :op-visited? (conj op-visited? op-id)})))
-          {:acc 0 :op-id 0 :op-visited? #{}}
-          (repeat nil)))
-
-(defn acc-after-program-terminates
-  "
-  state machine을 계속 실행하면서 프로그램이 마지막 instruction에 도달하였으면 그때의 최종 acc값을 반환하고,
-  loop가 발견되었다면 nil을 반환합니다.
-  Input: [[:acc 10] [:acc -1] [:nop 10]]
-  Output: 9
-
-  Input: [[:acc 10] [:jmp -1] [:nop 10]]
-  Output: nil
-  "
-  [ops]
-  (loop [acc 0 op-id 0 op-visited? #{}]
-    (let [{new-acc :acc new-op-id :op-id} (next-step {:acc acc :op-id op-id :ops ops})]
-      (cond
-        (op-visited? new-op-id) nil
-        (>= new-op-id (count ops)) new-acc
-        :else (recur new-acc new-op-id (conj op-visited? op-id))))))
+      :acc {:acc (+ acc arg)
+            :op-id (inc op-id)
+            :ops ops
+            :history (conj history op-id)}
+      :nop {:acc acc
+            :op-id (inc op-id)
+            :ops ops
+            :history (conj history op-id)}
+      :jmp {:acc acc
+            :op-id (+ op-id arg)
+            :ops ops
+            :history (conj history op-id)})))
 
 (defn swap-jmp-and-nop-at
   "ops 벡터에서 idx 위치에 있는 instruction이 jmp면 nop로, nop면 jmp로 바꿔 줍니다.
@@ -67,11 +36,11 @@
   Output: [[:acc 10] [:acc -1] [:jmp 10]]
   "
   [idx ops]
-  (let [[op arg] (nth ops idx)
+  (let [{:keys [op arg]} (nth ops idx)
         new-op (case op
                  :jmp :nop
                  :nop :jmp)]
-    (assoc ops idx [new-op arg])))
+    (assoc ops idx {:op new-op :arg arg})))
 
 (defn get-indices-of-jmp-or-nop
   "ops 벡터에서 instruction이 jmp나 nop인 것들의 인덱스를 반환합니다.
@@ -81,7 +50,7 @@
   [ops]
   (->> ops
        (map-indexed vector)
-       (filter (fn [[idx [op arg]]] (or (= op :nop) (= op :jmp))))
+       (filter (fn [[idx {:keys [op arg]}]] (or (= op :nop) (= op :jmp))))
        (map first)))
 
 (defn generate-candidate-ops
@@ -90,19 +59,43 @@
   Output: [[[:acc 10] [:nop -1] [:nop 10]] [[:acc 10] [:jmp -1] [:jmp 10]]]
   "
   [ops]
-  (let [jmp-or-nop-indices (get-indices-of-jmp-or-nop ops)]
-    (for [idx jmp-or-nop-indices] (swap-jmp-and-nop-at idx ops))))
+  (for [idx (get-indices-of-jmp-or-nop ops)] (swap-jmp-and-nop-at idx ops)))
+
+(defn loop-detected? [{:keys [op-id history]}]
+  (contains? history op-id))
+
+(defn reached-end? [{:keys [op-id ops]}]
+  (>= op-id (count ops)))
+
+(defn not-loop-detected-and-not-reached-end? [state]
+  (and
+    (not (loop-detected? state))
+    (not (reached-end? state))))
+
+(defn get-acc-if-reached-end [{:keys [acc op-id ops history] :as state}]
+  (cond
+    (loop-detected? state) nil
+    (reached-end? state) acc))
+
+(defn acc-after-program-terminates [ops]
+  (->>
+    (#(iterate next-step {:acc 0 :op-id 0 :ops ops :history #{}}))
+    (drop-while not-loop-detected-and-not-reached-end?)
+    first
+    get-acc-if-reached-end))
 
 (comment
   ; Part 1
   (->> input-val
        (map parse)
-       do-until-loop-detected)
+       (#(iterate next-step {:acc 0 :op-id 0 :ops % :history #{}}))
+       (drop-while loop-detected?)
+       first
+       :acc)
 
   ;Part 2
   (->> input-val
-       (map parse)
-       vec
+       (mapv parse)
        generate-candidate-ops
        (keep acc-after-program-terminates)
        first))
