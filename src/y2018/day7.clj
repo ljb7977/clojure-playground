50a68dcccfc8e33b8150f1e939cbf8f909a53ba8
Jubeen Lee <ljb7977@gmail.com>
2021-12-17 19:27:37 +0900

    init 함수 추가

--- src/y2018/day7.clj
+++ src/y2018/day7.clj
@@ -69,42 +69,6 @@
         (apply dissoc m jobs)
         (remove-values m jobs)))
 
-(defn process-one-step
-  "Input: {:removed-keys [] :graph {A #{C}, B #{A}, C #{}, D #{A}, E #{B D F}, F #{C}}}
-  Output: {:removed-keys [C] :graph {A #{}, B #{A}, D #{A}, E #{B D F}, F #{}}}
-  "
-  [{:keys [removed-keys graph] :as all}]
-  (if (empty? graph)
-    all
-    (let [key-to-remove (->> graph
-                             get-keys-where-value-is-empty
-                             (apply min-key int))]
-      {:removed-keys (conj removed-keys key-to-remove)
-       :graph        (remove-jobs-from-graph graph [key-to-remove])})))
-;(process-one-step {:result [], :graph {\A #{\C}, \B #{\A}, \C #{}, \D #{\A}, \E #{\B \D \F}, \F #{\C}}})
-
-; 로직:
-; 1. 모든 워커들에 대해 1초식 진행시켜준다.
-; workers: 알파벳 -> 남은 시간의 map
-; == (fmap dec workers)
-(defn process-workers-one-step
-  "Input: {A 5, B 1}
-  Output: {A 4, B 0}"
-  [workers]
-  (->> workers
-       (map (fn [[job remaining]] [job (dec remaining)]))
-       (into {})))
-
-; 2. 끝난 워커가 있으면, 그 job을 회수한다.
-(defn reap-finished-job
-  "Input: {A 0, B 1}
-  Output: {:workers {B 1} :finished-jobs (A)}"
-  [workers]
-  (let [{finished-workers true
-         ongoing-workers  false} (group-by #(= 0 (val %)) workers)]
-    {:finished-jobs (keys finished-workers)
-     :workers       (into {} ongoing-workers)}))
-
 (defn get-duration-of-job
   "알파벳 job이 들어오면 그 job을 완료하는데 걸리는 시간을 구합니다.
   Input: A 61
@@ -112,6 +76,9 @@
   [job offset]
   (+ offset (- (int job) (int \A))))
 
+; 로직:
+; workers: 알파벳 -> 남은 시간의 map
+; 1. 넣을 수 있는 만큼 새 알파벳을 워커에 넣는다.
 (defn assign-jobs-to-workers
   "현재 graph에서 처리할 수 있는 job들을 최대한 worker에 할당합니다."
   [workers graph max-workers duration-offset]
@@ -127,9 +94,28 @@
             assignable-jobs)))
 (assign-jobs-to-workers {} {\A #{\C}, \B #{\A}, \C #{}, \D #{\A}, \E #{\B \D \F}, \F #{\C}} 5 61)
 
-; 3. 처리 완료된 알파벳들을 result 리스트에 추가한다.
-; 4. 그래프에서 그 알파벳들을 제거한다.
-; 5. 넣을 수 있는 만큼 새 알파벳을 워커에 넣는다.
+; 2. 모든 워커들에 대해 1초식 진행시켜준다.
+; == (fmap dec workers)
+(defn process-workers-one-step
+  "Input: {A 5, B 1}
+  Output: {A 4, B 0}"
+  [workers]
+  (->> workers
+       (map (fn [[job remaining]] [job (dec remaining)]))
+       (into {})))
+
+; 3. 끝난 워커가 있으면, 그 job을 회수한다.
+(defn reap-finished-job
+  "Input: {A 0, B 1}
+  Output: {:workers {B 1} :finished-jobs (A)}"
+  [workers]
+  (let [{finished-workers true
+         ongoing-workers  false} (group-by #(= 0 (val %)) workers)]
+    {:finished-jobs (keys finished-workers)
+     :workers       (into {} ongoing-workers)}))
+
+; 4. 처리 완료된 알파벳들을 result 리스트에 추가한다.
+; 5. 그래프에서 그 알파벳들을 제거한다.
 (defn process-step-with-workers
   "worker들을 이용한 한 스텝을 진행합니다.
   먼저 주어진 workers에서 처리 완료된 job들을 모두 수거하고, finished-jobs에 추가합니다.
@@ -166,26 +152,34 @@
 ;                            :duration-offset 61
 ;                            :elapsed-time 0})
 
-(defn get-result-as-string [{result :removed-keys}]
+(defn init [{:keys [max-workers duration-offset]} graph]
+  {:graph graph
+   :workers {}
+   :result ()
+   :elapsed-time 0
+   :max-workers max-workers
+   :duration-offset duration-offset})
+
+(defn ongoing? [{:keys [workers graph]}]
+  (or (seq workers) (seq graph)))
+
+(defn get-result-as-string [{result :result}]
   (apply str result))
 
 (comment
   ; Part 1
   (->> input-val
        parse
-       (#(iterate process-one-step {:removed-keys [] :graph %}))
-       (drop-while #(seq (:graph %)))
+       (init {:max-workers 1 :duration-offset 1})
+       (iterate process-step-with-workers)
+       (drop-while ongoing?)
        first
        get-result-as-string)
   ; Part 2
-  (let [initial-state {:graph           (parse input-val)
-                       :workers         {}
-                       :result          []
-                       :max-workers     5
-                       :duration-offset 61
-                       :elapsed-time    0}]
-    (->> initial-state
-         (iterate process-step-with-workers)
-         (drop-while #(or (seq (:workers %)) (seq (:graph %))))
-         first
-         :elapsed-time)))
+  (->> input-val
+       parse
+       (init {:max-workers 5 :duration-offset 61})
+       (iterate process-step-with-workers)
+       (drop-while ongoing?)
+       first
+       :elapsed-time))
