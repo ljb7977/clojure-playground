30880cfe43985480d2b74dae9e313690d67623ce
Jubeen Lee <ljb7977@gmail.com>
2022-11-01 01:09:52 +0900

    tap>> 함수 추가

--- src/dev_dive/07_tap_let.clj
+++ src/dev_dive/07_tap_let.clj
@@ -2,11 +2,33 @@
   (:require [pez.taplet :refer [let>]]))
 
 (comment
+  ;; let-tap
   (let> [a 1
          b :keyword
          c "string"
          {name :name
-          age :age
-          :as d} {:name "Harry"
-                  :surname "Porter"
-                  :age 20}]))
+          age  :age
+          :as  d} {:name    "Harry"
+                   :surname "Porter"
+                   :age     20}]
+    [a b c d name age]))
+
+(defn tap>> [x]
+  (tap> x)
+  x)
+
+(comment
+  ;; convenient tap> in threading macro
+  (let [t [{:subject :math
+            :score 100
+            :grade "A+"}
+           {:subject :english
+            :score 90
+            :grade "A"}
+           {:subject :science
+            :score 80
+            :grade "B"}]]
+    (->> t
+         (filter #(> (:score %) 80))
+         (tap>>)
+         (map :score))))
