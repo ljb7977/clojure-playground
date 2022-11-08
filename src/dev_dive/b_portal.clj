346985e8ccea8f445cb2400be4d6f2398c1cd5ab
Jubeen Lee <ljb7977@gmail.com>
2022-11-08 17:50:02 +0900

    Add some data

--- src/dev_dive/b_portal.clj
+++ src/dev_dive/b_portal.clj
@@ -1,5 +1,6 @@
 (ns dev-dive.b-portal
-  (:require [dev-dive.data :refer [example-products products-from-db]]))
+  (:require [dev-dive.data :refer [example-products products-from-db
+                                   portal-example portal-string-data]]))
 
 ;; ->tap 정의
 (defn ->tap [x]
@@ -26,19 +27,9 @@
          :job "Programmer"
          :language "Clojure"})
   (tap> example-products)
+  (tap> portal-example)
+  (tap> portal-string-data)
 
-  '("상추" "바나나" "사과" "양배추")
-
-  ;; 2. 스레딩 매크로
-  ;(->> example-products
-  ;     (map (fn [{:keys [prev-price price] :as product}] (assoc product :price-diff (- price prev-price))))
-  ;     (map (fn [{:keys [prev-price price-diff] :as product}] (assoc product :price-diff-rate (* (/ price-diff prev-price) 1.0))))
-  ;     (filter (fn [{:keys [price-diff-rate]}] (> price-diff-rate 0.1)))
-  ;     (sort-by :price-diff-rate >)
-  ;     (map :name)
-  ;     ,)
-
-  (top-price-increased-products example-products)
 
   ;; 3. 많은 양의 데이터
   (def more-products (products-from-db))
