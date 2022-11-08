74e543fb689eaab97bd59eb4ebeca34be71f8333
Jubeen Lee <ljb7977@gmail.com>
2022-11-08 17:00:14 +0900

    save

--- src/dev_dive/a_repl_basics.clj
+++ src/dev_dive/a_repl_basics.clj
@@ -1,32 +1,16 @@
 (ns dev-dive.a-repl-basics
   (:require [dev-dive.data :refer [example-products products-from-db]]))
 
-(defn top-price-increased-products [products]
-  (->> products
-       (map (fn [{:keys [prev-price price] :as product}]
-              (assoc product :price-diff (- price prev-price))))
-       (map (fn [{:keys [prev-price price-diff] :as product}]
-              (assoc product :price-diff-rate (* (/ price-diff prev-price) 100.0))))
-       (filter (fn [{:keys [price-diff-rate]}] (> price-diff-rate 10)))
-       (sort-by :price-diff-rate >)))
-       ;(map :name)))
+(defn top-price-increased-products [products])
 
 (comment
   ;; 0. 데이터 확인
   example-products
 
-  ;; 1. 시세의 변동폭이 20% 이상인 과일/채소들을, 변동폭이 높은 순서대로 이름을 구하기
-  (->> example-products
-       (map (fn [{:keys [prev-price price] :as product}]
-              (assoc product :price-diff (- price prev-price))))
-       (map (fn [{:keys [prev-price price-diff] :as product}]
-              (assoc product :price-diff-rate (* (/ price-diff prev-price) 100.0))))
-       (filter (fn [{:keys [price-diff-rate]}] (> price-diff-rate 10)))
-       (sort-by :price-diff-rate >)
-       (map :name)
-       ,)
+  ;; 1. 시세의 변동폭이 10% 이상인 과일/채소들을, 변동폭이 높은 순서대로 이름을 구하기
 
   ;; 2. 함수로 분리하기
+  (top-price-increased-products example-products)
 
   ;; 3. 더 많은 데이터에 대해 적용
   (def more-products (products-from-db))
