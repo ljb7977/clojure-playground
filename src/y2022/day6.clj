79f97a98ef64d319dc3458aa1078f25f10591ced
Jubeen Lee <ljb7977@gmail.com>
2022-12-21 21:29:17 +0900

    day6

--- src/y2022/day6.clj
+++ src/y2022/day6.clj
@@ -1,3 +1,24 @@
 (ns y2022.day6)
 
-(def input (slurp ""))
+(def input (slurp "src/y2022/day6.txt"))
+
+(defn all-different? [s]
+  (let [len (count s)]
+    (= len (-> s set count))))
+
+(defn solve [input n]
+  (let [len (- (count input) n)]
+    (->> (range len)
+         (map #(subs input % (+ % n)))
+         (take-while (complement all-different?))
+         count
+         (+ n))))
+
+(comment
+  ;; Part 1
+  (solve input 4)
+  := 1566
+
+  (solve input 14)
+  := 2265
+  :rcf)
