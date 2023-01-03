e30c97f9e682eb8edbc48b855d10cb384fdb59d4
Jubeen Lee <ljb7977@gmail.com>
2023-01-04 01:34:28 +0900

    Part 1 done

--- src/y2022/day7.clj
+++ src/y2022/day7.clj
@@ -1,6 +1,6 @@
 (ns y2022.day7
   (:require [clojure.core.match :refer [match]]
-            [clojure.walk :refer [postwalk]]))
+            [clojure.walk :refer [postwalk postwalk-demo]]))
 
 (def input (slurp "src/y2022/day7.txt"))
 
@@ -35,29 +35,35 @@
              :file-tree file-tree}))
     :ls {:current-dir current-dir
          :file-tree file-tree}
-    :dir {:current-dir current-dir
-          :file-tree (assoc-in file-tree (conj current-dir (:name val)) val)}
-    :file {:current-dir current-dir
-           :file-tree (assoc-in file-tree (conj current-dir (:name val)) val)}))
+    (:dir :file) (let [path (conj current-dir (:name val))]
+                   {:current-dir current-dir
+                    :file-tree (assoc-in file-tree path {:info val})})))
 
 (defn walk-calc-size [file-tree]
-  (postwalk (fn [sub-file-tree]
-              (if (not (map? sub-file-tree))
-                sub-file-tree
-                (case (:type sub-file-tree)
-                  :file sub-file-tree
-                  :dir (let [size (->> sub-file-tree
-                                       vals
-                                       (filter map?)
-                                       (map :size)
-                                       (apply +))]
-                         (assoc sub-file-tree :size size))
-                  sub-file-tree)))
-                  ;(do (prn sub-file-tree)
-                  ;    sub-file-tree))))
+  (postwalk (fn [node]
+              (if (not (:info node)) ; info가 없으면 더 밑으로 내려가면 안 됨
+                node
+                (case (get-in node [:info :type])
+                  :file node
+                  :dir (let [size (->> node
+                                        (filter #(not= (key %) :info))
+                                        vals
+                                        (map #(get-in % [:info :size]))
+                                        (apply +))]
+                          (assoc-in node [:info :size] size))
+                  (throw (ex-info "unknown" {:node node})))))
             file-tree))
 
-(defn reap-sizes-of-dirs [{}])
+(defn collect-dir-sizes [tree]
+  (case (get-in tree [:info :type])
+    :file []
+    :dir (let [children (->> tree
+                             (filter #(not= (key %) :info))
+                             vals)
+               size (get-in tree [:info :size])]
+           (conj (mapcat collect-dir-sizes children) size))
+    (throw (ex-info "no matchin clause" {:node tree
+                                         :type (get-in tree [:info :type])}))))
 
 (comment
   {:type :cd, :dirname "/"}
@@ -78,12 +84,16 @@
   (scan-command-result {:current-dir ["/"], :file-tree {"/" {"a" {:type :dir, :name "a"}}}}
                        {:type :file, :name "b.txt", :size 14848514})
   
-  (let [size-calced (->> (clojure.string/split-lines sample-input)
+  (let [size-calced (->> (clojure.string/split-lines input)
                          (map parse-line)
                          (reduce scan-command-result {:current-dir []
-                                                      :file-tree {"/" {:type :dir
-                                                                       :name "/"}}})
+                                                      :file-tree {"/" {:info {:type :dir
+                                                                              :name "/"}}}})
                          :file-tree
-                         walk-calc-size)]
-    (get size-calced "/")))
-;
+                         walk-calc-size)
+        dir-sizes (-> size-calced
+                      (get "/")
+                      collect-dir-sizes)]
+    (->> dir-sizes
+         (filter #(<= % 100000))
+         (apply +))))
